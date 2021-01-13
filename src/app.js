fd0d162378b62dfa40ecb973bb212ef16fbc9ecb
Ivy Benson <irosebenson@gmail.com>
2021-01-12 20:02:07 -0600

    additional setup

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(helmet());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Hello, dnd logger!");
 });
 
 app.use(function errorHandler(error, req, res, next) {
