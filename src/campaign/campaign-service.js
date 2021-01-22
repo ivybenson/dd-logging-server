3d668ff4d6b2e03534cd0f616083b6409c7ed7c0
Ivy Benson <irosebenson@gmail.com>
2021-01-22 16:24:40 -0600

    updates to migrations

---
+++ src/campaign/campaign-service.js
@@ -0,0 +1,34 @@
+const campaignService = {
+  getAllCampaigns(knex) {
+    return knex.select("*").from("campaigns");
+  },
+
+  getCampaignById(knex, id) {
+    return knex.from("campaigns").select("*").where({ id }).first();
+  },
+
+  getCampaignsByUser(knex, user_id) {
+    return knex
+      .from("campaigns")
+      .select("*")
+      .where({ user_id })
+      .orderBy("created", "desc");
+  },
+
+  insertCampaign(knex, newCampaign) {
+    return knex
+      .insert(newCampaign)
+      .into("campaigns")
+      .returning("*")
+      .then((rows) => rows[0]);
+  },
+
+  deleteCampaign(knex, id) {
+    return knex("campaigns").where({ id }).delete();
+  },
+  updateCampaign(knex, id, newCampaignFields) {
+    return knex("campaigns").where({ id }).update(newCampaignFields);
+  },
+};
+
+module.exports = campaignService;
